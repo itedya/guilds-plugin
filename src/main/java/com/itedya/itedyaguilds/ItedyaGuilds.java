5d7b42bffffd11b857c8f3c005b0fcf1a6e91860
itedya <kontakt@itedya.com>
2022-01-22 00:01:11 +0100

    Removed useless launch logs and changed version

--- src/main/java/com/itedya/itedyaguilds/ItedyaGuilds.java
+++ src/main/java/com/itedya/itedyaguilds/ItedyaGuilds.java
@@ -16,14 +16,11 @@ public final class ItedyaGuilds extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        this.logger.info("[ItedyaGuilds] Creating data folder");
         this.getDataFolder().mkdir();
 
         // initializing database
         try {
-            this.logger.info("[ItedyaGuilds] Connecting to database");
             Database.connectToDatabase(this);
-            this.logger.info("[ItedyaGuilds] Migrating database");
             Database.migrateDatabase(this);
         } catch (SQLException e) {
             this.logger.severe(e.getMessage());
@@ -33,35 +30,23 @@ public final class ItedyaGuilds extends JavaPlugin {
             Bukkit.getPluginManager().disablePlugin(this);
         }
 
-        this.logger.info("[ItedyaGuilds] Initializing invites controller");
         InvitesController.initialize(this);
-        this.logger.info("[ItedyaGuilds] Initializing config controller");
         ConfigController.initialize(this);
 
         // Initialize commands
-        this.logger.info("[ItedyaGuilds] Initializing /akceptujzaproszenie command");
         AcceptInviteToGuild.initialize(this);
-        this.logger.info("[ItedyaGuilds] Initializing /stworzgildie command");
         CreateGuild.initialize(this);
-        this.logger.info("[ItedyaGuilds] Initializing /usungildie command");
         DeleteGuild.initialize(this);
-        this.logger.info("[ItedyaGuilds] Initializing /wyjdzzgildii command");
         ExitFromGuild.initialize(this);
-        this.logger.info("[ItedyaGuilds] Initializing /zaproszgildii command");
         InvitePlayerToGuild.initialize(this);
-        this.logger.info("[ItedyaGuilds] Initializing /wyrzuczgildii command");
         KickOutOfGuild.initialize(this);
-        this.logger.info("[ItedyaGuilds] Initializing /ustawdomgildii command");
         SetGuildHome.initialize(this);
-        this.logger.info("[ItedyaGuilds] Initializing /domgildii command");
         TeleportToGuildHome.initialize(this);
 
 
         // Initializing cuboid boss bar
-        this.logger.info("[ItedyaGuilds] Initializing cuboid boss bar");
         Bukkit.getServer().getPluginManager().registerEvents(new DisplayCuboidInfoListener(), this);
 
-        this.logger.info("[ItedyaGuilds] Initializing papi expander");
         if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
             new PlaceholderAPIExpansion().register();
         }
