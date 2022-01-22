9a9ece8abe6f93551c0ba39280e4fbe1754fde9a
itedya <kontakt@itedya.com>
2022-01-22 21:17:01 +0100

    Changed version to 1.3.1

--- src/main/java/com/itedya/itedyaguilds/PlaceholderAPIExpansion.java
+++ src/main/java/com/itedya/itedyaguilds/PlaceholderAPIExpansion.java
@@ -23,7 +23,7 @@ public class PlaceholderAPIExpansion extends PlaceholderExpansion {
 
     @Override
     public @NotNull String getVersion() {
-        return "1.3";
+        return "1.3.1";
     }
 
     @Override
