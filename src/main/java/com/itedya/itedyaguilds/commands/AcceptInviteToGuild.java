57dc398ef47f3230011163e240ded2821bab9f6a
itedya <kontakt@itedya.com>
2022-01-27 12:15:21 +0100

    Adopted /g akceptuj command to new command utility

--- src/main/java/com/itedya/itedyaguilds/commands/AcceptInviteToGuild.java
+++ src/main/java/com/itedya/itedyaguilds/commands/AcceptInviteToGuild.java
@@ -1,14 +1,18 @@
 package com.itedya.itedyaguilds.commands;
 
 import com.itedya.itedyaguilds.controllers.*;
+import com.itedya.itedyaguilds.exceptions.NotEnoughPermissionsException;
+import com.itedya.itedyaguilds.exceptions.PlayerDoesNotHaveInviteToGuildException;
+import com.itedya.itedyaguilds.exceptions.PlayerIsAlreadyInGuildException;
 import com.itedya.itedyaguilds.models.Guild;
-import com.itedya.itedyaguilds.models.GuildMember;
-import org.bukkit.ChatColor;
+import com.itedya.itedyaguilds.utils.CommandUtil;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
+import java.sql.SQLException;
+import java.text.ParseException;
 import java.util.logging.Logger;
 
 public class AcceptInviteToGuild {
@@ -18,45 +22,44 @@ public class AcceptInviteToGuild {
         logger = plugin.getLogger();
     }
 
-    public boolean onCommand(@NotNull Player player, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        try {
-            if (!player.hasPermission("itedya-guilds.accept-invite")) {
-                player.sendMessage(MessagesController.getMessage("not_enough_permissions"));
-                return true;
-            }
+    public boolean onCommand(@NotNull Player player) throws SQLException, PlayerIsAlreadyInGuildException, NotEnoughPermissionsException, ParseException, PlayerDoesNotHaveInviteToGuildException {
+        CommandUtil.playerMustHavePermission(player, "itedya-guilds.accept-invite");
+        CommandUtil.playerCantBeInGuild(player);
 
-            Guild guild = GuildsController.getPlayerGuild(player);
-            if (guild != null) {
-                player.sendMessage(MessagesController.getMessage("you_are_already_in_guild"));
-                return true;
-            }
+        Guild guild = InvitesController.getGuildThatInvitesPlayer(player);
 
-            guild = InvitesController.getGuildThatInvitesPlayer(player);
+        if (guild == null) throw new PlayerDoesNotHaveInviteToGuildException();
 
-            if (guild == null) {
-                player.sendMessage(MessagesController.getMessage("you_dont_have_invite_to_guild"));
-                return true;
-            }
+        GuildsController.addPlayerToGuild(player, guild, "MEMBER");
+        WorldGuardController.addPlayerToGuildCuboid(player, guild);
 
-            GuildsController.addPlayerToGuild(player, guild, "MEMBER");
-            WorldGuardController.addPlayerToGuildCuboid(player, guild);
-            player.sendMessage(MessagesController.getMessage("welcome_to_guild")
-                    .replaceAll("\\{GUILD_NAME}", guild.name)
-                    .replaceAll("\\{GUILD_SHORT_NAME}", guild.short_name));
+        player.sendMessage(MessagesController.getMessage("welcome_to_guild")
+                .replaceAll("\\{GUILD_NAME}", guild.name)
+                .replaceAll("\\{GUILD_SHORT_NAME}", guild.short_name));
 
-            this.logger.info("User " + player.getName() + " " + player.getUniqueId() + " " +
-                    "accepted invite to guild " + guild.uuid.toString() + " [" + guild.short_name + "] " + guild.name);
-
-            var members = guild.getMembers();
-            for (var member : members) {
-                member.player.sendMessage(MessagesController.getMessage("player_has_been_added_to_guild")
-                        .replaceAll("\\{PLAYER_NAME}", player.getName()));
-            }
-        } catch (Exception e) {
-            player.sendMessage(MessagesController.getMessage("server_error"));
-            e.printStackTrace();
-        }
+        this.logCommandExecution(player, guild);
+        this.sendAnnouncement(player, guild);
 
         return true;
+    }
+
+    public void logCommandExecution(Player player, Guild guild) {
+        var message = "User ? ? accepted invite to guild ? [?] ?";
+
+        this.logger.info(message
+                .replace("?", player.getName())
+                .replace("?", player.getUniqueId().toString())
+                .replace("?", guild.uuid.toString())
+                .replace("?", guild.short_name)
+                .replace("?", guild.name));
+    }
+
+    public void sendAnnouncement(Player player, Guild guild) throws SQLException {
+        var members = guild.getMembers();
+
+        for (var member : members) {
+            member.player.sendMessage(MessagesController.getMessage("player_has_been_added_to_guild")
+                    .replaceAll("\\{PLAYER_NAME}", player.getName()));
+        }
     }
 }
