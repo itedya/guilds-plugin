2e739d84b1321d8c43a1682ab6f2377db728f2c7
itedya <kontakt@itedya.com>
2022-01-27 16:17:14 +0100

    Created new command utilities

--- src/main/java/com/itedya/itedyaguilds/utils/CommandUtil.java
+++ src/main/java/com/itedya/itedyaguilds/utils/CommandUtil.java
@@ -3,9 +3,13 @@ package com.itedya.itedyaguilds.utils;
 import com.itedya.itedyaguilds.controllers.GuildsController;
 import com.itedya.itedyaguilds.exceptions.NotEnoughPermissionsException;
 import com.itedya.itedyaguilds.exceptions.PlayerIsAlreadyInGuildException;
+import com.itedya.itedyaguilds.exceptions.PlayerMustBeInGuildException;
+import com.itedya.itedyaguilds.exceptions.PlayerMustBeOwnerOfGuildException;
+import com.itedya.itedyaguilds.models.Guild;
 import org.bukkit.entity.Player;
 
 import java.sql.SQLException;
+import java.text.ParseException;
 
 public class CommandUtil {
     private CommandUtil() throws Exception {
@@ -34,8 +38,20 @@ public class CommandUtil {
      * @throws PlayerIsAlreadyInGuildException
      */
     public static void playerCantBeInGuild(Player player) throws SQLException, PlayerIsAlreadyInGuildException {
-        if (!GuildsController.isPlayerInGuild(player)) {
+        if (GuildsController.isPlayerInGuild(player)) {
             throw new PlayerIsAlreadyInGuildException();
+        }
+    }
+
+    public static void playerMustBeInGuild(Player player) throws SQLException, PlayerMustBeInGuildException {
+        if (!GuildsController.isPlayerInGuild(player)) {
+            throw new PlayerMustBeInGuildException();
+        }
+    }
+
+    public static void playerMustBeOwnerOfGuild(Player player, Guild guild) throws SQLException, ParseException, PlayerMustBeOwnerOfGuildException {
+        if (!GuildsController.isPlayerOwnerOfGuild(player, guild)) {
+            throw new PlayerMustBeOwnerOfGuildException();
         }
     }
 }
