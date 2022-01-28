3e630316c94322c473fdd3eab23675b4569019b8
itedya <kontakt@itedya.com>
2022-01-28 20:48:56 +0100

    Fixed properties in member model

--- src/main/java/com/itedya/itedyaguilds/models/Member.java
+++ src/main/java/com/itedya/itedyaguilds/models/Member.java
@@ -1,24 +1,45 @@
 package com.itedya.itedyaguilds.models;
 
+import com.itedya.itedyaguilds.enums.MemberRole;
+
+import java.util.Date;
 import java.util.UUID;
 
 public class Member {
-    private UUID uuid;
-    private Guild guild;
+    private UUID playerUuid;
+    private Integer guildId;
+    private MemberRole role;
+    private Date createdAt;
 
-    public UUID getUuid() {
-        return uuid;
+    public UUID getPlayerUuid() {
+        return playerUuid;
     }
 
-    public void setUuid(UUID uuid) {
-        this.uuid = uuid;
+    public void setPlayerUuid(UUID playerUuid) {
+        this.playerUuid = playerUuid;
     }
 
-    public Guild getGuild() {
-        return guild;
+    public Integer getGuildId() {
+        return guildId;
     }
 
-    public void setGuild(Guild guild) {
-        this.guild = guild;
+    public void setGuildId(Integer guildId) {
+        this.guildId = guildId;
+    }
+
+    public MemberRole getRole() {
+        return role;
+    }
+
+    public void setRole(MemberRole role) {
+        this.role = role;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
     }
 }
